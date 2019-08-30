aa5820791124f17488e5742e7eeaa899841071f5
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-08-30 11:53:40 +0200

    Hello World! You can start typing.

--- app/routes/editor/new-document.js
+++ app/routes/editor/new-document.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  model(){
+    //The prefix and vocab attributes are defined here manually. You will always have to provide them to the editor.
+    return `
+    <h1>Hello World!</h1>
+   `;
+  }
 });
